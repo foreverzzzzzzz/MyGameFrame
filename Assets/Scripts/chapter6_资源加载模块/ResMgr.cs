e4c17a1682292f31a9b636df5dc665e78d3d3756
foreverzzzzzzz <15834063946@163.com>
2026-06-28 15:25:56 +0800

    Resources资源加载模块进阶优化 引用计数

--- Assets/Scripts/chapter6_资源加载模块/ResMgr.cs
+++ Assets/Scripts/chapter6_资源加载模块/ResMgr.cs
@@ -7,7 +7,10 @@ using UnityEngine.Events;
 /// <summary>
 /// 资源信息基类 主要用于里式替换原则 父类容器装子类对象
 /// </summary>
-public abstract class ResInfoBase { }
+public abstract class ResInfoBase {
+    //引用计数
+    public int refCount;
+}
 
 /// <summary>
 /// 资源信息对象 主要用于存储资源信息 异步加载委托信息 异步加载 协程信息
@@ -21,17 +24,34 @@ public class ResInfo<T> : ResInfoBase
     public UnityAction<T> callBack;
     //用于存储异步加载时 开启的协同程序
     public Coroutine coroutine;
-    //是否需要移除
+    //决定引用计数为0时 是否真正需要移除
     public bool isDel;
+    
+
+    public void AddRefCount()
+    {
+        ++refCount;
+    }
+
+    public void SubRefCount()
+    {
+        --refCount;
+        if (refCount < 0)
+            Debug.LogError("引用计数小于0了，请检查使用和卸载是否配对执行");
+    }
 }
 
+
+/// <summary>
+/// Resources 资源加载模块管理器
+/// </summary>
 public class ResMgr : BaseManager<ResMgr>
 {
     //用于存储加载过的资源或者加载中的资源的容器
     private Dictionary<string, ResInfoBase> resDic = new Dictionary<string, ResInfoBase>();
-    
+
     private ResMgr() { }
-    
+
     /// <summary>
     /// 同步加载Resources下资源的方法
     /// </summary>
@@ -50,6 +70,8 @@ public class ResMgr : BaseManager<ResMgr>
             T res = Resources.Load<T>(path);
             info = new ResInfo<T>();
             info.asset = res;
+            //引用计数增加
+            info.AddRefCount();
             resDic.Add(resName, info);
             return res;
         }
@@ -57,6 +79,8 @@ public class ResMgr : BaseManager<ResMgr>
         {
             //取出字典中的记录
             info = resDic[resName] as ResInfo<T>;
+            //引用计数增加
+            info.AddRefCount();
             //存在异步加载 还在加载中
             if(info.asset == null)
             {
@@ -80,9 +104,8 @@ public class ResMgr : BaseManager<ResMgr>
                 return info.asset;
             }
         }
-
     }
-    
+
     /// <summary>
     /// 异步加载资源的方法
     /// </summary>
@@ -98,6 +121,8 @@ public class ResMgr : BaseManager<ResMgr>
         {
             //声明一个 资源信息对象
             info = new ResInfo<T>();
+            //引用计数增加
+            info.AddRefCount();
             //将资源记录添加到字典中（资源还没有加载成功）
             resDic.Add(resName, info);
             //记录传入的委托函数 一会儿加载完成了 再使用
@@ -109,6 +134,8 @@ public class ResMgr : BaseManager<ResMgr>
         {
             //从字典中取出资源信息
             info = resDic[resName] as ResInfo<T>;
+            //引用计数增加
+            info.AddRefCount();
             //如果资源还没有加载完 
             //意味着 还在进行异步加载
             if (info.asset == null)
@@ -117,7 +144,6 @@ public class ResMgr : BaseManager<ResMgr>
             else
                 callBack?.Invoke(info.asset);
         }
-
     }
 
     private IEnumerator ReallyLoadAsync<T>(string path) where T : UnityEngine.Object
@@ -127,7 +153,7 @@ public class ResMgr : BaseManager<ResMgr>
         //等待资源加载结束后 才会继续执行yield return后面的代码
         //此处不返回任何东西，只是将rq交给协程调度器，每一帧都会检测rq.isDone 是否为 true，如果是才继续向后执行
         yield return rq;
-        
+
         string resName = path + "_" + typeof(T).Name;
         //资源加载结束 将资源传到外部的委托函数去进行使用
         if (resDic.ContainsKey(resName))
@@ -135,11 +161,11 @@ public class ResMgr : BaseManager<ResMgr>
             ResInfo<T> resInfo = resDic[resName] as ResInfo<T>;
             //取出资源信息 并且记录加载完成的资源
             resInfo.asset = rq.asset as T;
-            //将加载完成的资源传递出去 将存入的callBack一起调用
-            //资源加载结束 将资源传到外部的委托函数去进行使用 真正的资源存储在rq.asset中
+            
             //如果发现需要删除 再去移除资源
-            if (resInfo.isDel)
-                UnloadAsset<T>(path);
+            //引用计数为0 才真正去移除
+            if (resInfo.refCount == 0)
+                UnloadAsset<T>(path, resInfo.isDel, null, false);
             else
             {
                 //将加载完成的资源传递出去
@@ -148,11 +174,10 @@ public class ResMgr : BaseManager<ResMgr>
                 resInfo.callBack = null;
                 resInfo.coroutine = null;
             }
-
         }
-
+        
     }
-    
+
     /// <summary>
     /// 异步加载资源的方法
     /// </summary>
@@ -168,6 +193,8 @@ public class ResMgr : BaseManager<ResMgr>
         {
             //声明一个 资源信息对象
             info = new ResInfo<UnityEngine.Object>();
+            //引用计数增加
+            info.AddRefCount();
             //将资源记录添加到字典中（资源还没有加载成功）
             resDic.Add(resName, info);
             //记录传入的委托函数 一会儿加载完成了 再使用
@@ -179,6 +206,8 @@ public class ResMgr : BaseManager<ResMgr>
         {
             //从字典中取出资源信息
             info = resDic[resName] as ResInfo<UnityEngine.Object>;
+            //引用计数增加
+            info.AddRefCount();
             //如果资源还没有加载完 
             //意味着 还在进行异步加载
             if (info.asset == null)
@@ -203,8 +232,9 @@ public class ResMgr : BaseManager<ResMgr>
             //取出资源信息 并且记录加载完成的资源
             resInfo.asset = rq.asset;
             //如果发现需要删除 再去移除资源
-            if (resInfo.isDel)
-                UnloadAsset(path, type);
+            //引用计数为0 才真正去移除
+            if (resInfo.refCount == 0)
+                UnloadAsset(path, type, resInfo.isDel, null, false);
             else
             {
                 //将加载完成的资源传递出去
@@ -213,64 +243,83 @@ public class ResMgr : BaseManager<ResMgr>
                 resInfo.callBack = null;
                 resInfo.coroutine = null;
             }
-
         }
     }
 
-    
     /// <summary>
-    /// 指定卸载一个资源
+    /// 卸载一个指定资源
     /// </summary>
-    /// <param name="assetToUnload"></param>
-    public void UnloadAsset<T>(string path)
+    /// <param name="path">资源路径</param>
+    /// <param name="isDel">是否立即删除</param>
+    /// <param name="callBack">回调函数</param>
+    /// <param name="isSub">引用计数是否减1</param>
+    /// <typeparam name="T"></typeparam>
+    public void UnloadAsset<T>(string path, bool isDel = false, UnityAction<T> callBack = null, bool isSub = true)
     {
         string resName = path + "_" + typeof(T).Name;
         //判断是否存在对应资源
         if(resDic.ContainsKey(resName))
         {
             ResInfo<T> resInfo = resDic[resName] as ResInfo<T>;
+            //引用计数-1
+            if(isSub)
+                resInfo.SubRefCount();
+            //记录 引用计数为0时  是否马上移除标签
+            resInfo.isDel = isDel;
             //资源已经加载结束 
-            if(resInfo.asset != null)
+            if(resInfo.asset != null && resInfo.refCount == 0 && resInfo.isDel)
             {
                 //从字典移除
                 resDic.Remove(resName);
                 //通过api 卸载资源
                 Resources.UnloadAsset(resInfo.asset as UnityEngine.Object);
             }
-            else//资源正在异步加载中
+            else if(resInfo.asset == null)//资源正在异步加载中
             {
                 //MonoMgr.Instance.StopCoroutine(resInfo.coroutine);
                 //resDic.Remove(resName);
                 //为了保险起见 一定要让资源移除了
                 //改变表示 待删除
-                resInfo.isDel = true;
+                //resInfo.isDel = true;
+                //当异步加载不想使用时 我们应该移除它的回调记录 而不是直接去卸载资源
+                //此处callback即加载资源时传入的callback
+                if (callBack != null)
+                    resInfo.callBack -= callBack;
+
             }
         }
     }
 
-    //实现非泛型加载时调用的卸载方法
-    public void UnloadAsset(string path, Type type)
+    public void UnloadAsset(string path, Type type, bool isDel = false, UnityAction<UnityEngine.Object> callBack = null, bool isSub = true)
     {
         string resName = path + "_" + type.Name;
         //判断是否存在对应资源
         if (resDic.ContainsKey(resName))
         {
             ResInfo<UnityEngine.Object> resInfo = resDic[resName] as ResInfo<UnityEngine.Object>;
+            //引用计数-1
+            if(isSub)
+                resInfo.SubRefCount();
+            //记录 引用计数为0时  是否马上移除标签
+            resInfo.isDel = isDel;
             //资源已经加载结束 
-            if (resInfo.asset != null)
+            if (resInfo.asset != null && resInfo.refCount == 0 && resInfo.isDel)
             {
                 //从字典移除
                 resDic.Remove(resName);
                 //通过api 卸载资源
                 Resources.UnloadAsset(resInfo.asset);
             }
-            else//资源正在异步加载中
+            else if (resInfo.asset == null)//资源正在异步加载中
             {
                 //MonoMgr.Instance.StopCoroutine(resInfo.coroutine);
                 //resDic.Remove(resName);
                 //为了保险起见 一定要让资源移除了
                 //改变表示 待删除
-                resInfo.isDel = true;
+                //resInfo.isDel = true;
+                //当异步加载不想使用时 我们应该移除它的回调记录 而不是直接去卸载资源
+                if (callBack != null)
+                    resInfo.callBack -= callBack;
             }
         }
     }
@@ -286,10 +335,57 @@ public class ResMgr : BaseManager<ResMgr>
 
     private IEnumerator ReallyUnloadUnusedAssets(UnityAction callBack)
     {
+        //就是在真正移除不使用的资源之前 应该把我们自己记录的那些引用计数为0 并且没有被移除记录的资源
+        //移除掉
+        List<string> list = new List<string>();
+        foreach (string path in resDic.Keys)
+        {
+            if (resDic[path].refCount == 0)
+                list.Add(path);
+        }
+        foreach (string path in list)
+        {
+            resDic.Remove(path);
+        }
+
         AsyncOperation ao = Resources.UnloadUnusedAssets();
         yield return ao;
         //卸载完毕后 通知外部
         callBack();
     }
 
+    /// <summary>
+    /// 获取当前某个资源的引用计数
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public int GetRefCount<T>(string path)
+    {
+        string resName = path + "_" + typeof(T).Name;
+        if(resDic.ContainsKey(resName))
+        {
+            return (resDic[resName] as ResInfo<T>).refCount;
+        }
+        return 0;
+    }
+
+
+    /// <summary>
+    /// 清空字典
+    /// </summary>
+    /// <param name="callBack"></param>
+    public void ClearDic(UnityAction callBack)
+    {
+        MonoMgr.Instance.StartCoroutine(ReallyClearDic(callBack));
+    }
+
+    private IEnumerator ReallyClearDic(UnityAction callBack)
+    {
+        resDic.Clear();
+        AsyncOperation ao = Resources.UnloadUnusedAssets();
+        yield return ao;
+        //卸载完毕后 通知外部
+        callBack();
+    }
 }
