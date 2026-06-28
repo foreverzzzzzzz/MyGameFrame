0224ebaffe3ff672804e787fad291211370fff21
foreverzzzzzzz <15834063946@163.com>
2026-06-28 18:23:28 +0800

    Assetbundle资源加载模块 ABMgr异步加载修改

--- Assets/Scripts/chapter6_资源加载模块/ABMgr.cs
+++ Assets/Scripts/chapter6_资源加载模块/ABMgr.cs
@@ -181,22 +181,59 @@ public class ABMgr : SingletonAutoMono<ABMgr>
     //正儿八经的 协程函数
     private IEnumerator ReallyLoadResAsync<T>(string abName, string resName, UnityAction<T> callBack) where T : Object
     {
-        //加载依赖包
-        LoadDependencies(abName);
+        //加载主包
+        LoadMainAB();
+        //获取依赖包
+        string[] strs = manifest.GetAllDependencies(abName);
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (!abDic.ContainsKey(strs[i]))
+            {
+                //一开始异步加载 就记录 如果此时的记录中的值 是null 那证明这个ab包正在被异步加载
+                abDic.Add(strs[i], null);
+                AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync(PathUrl + strs[i]);
+                yield return req;
+                //异步加载结束后 再替换之前的null  这时 不为null 就证明加载结束了
+                abDic[strs[i]] = req.assetBundle;
+            }
+            //就证明 字典中已经记录了一个AB包相关信息了
+            else
+            {
+                //如果字典中记录的信息是null 那就证明正在加载中
+                //我们只需要等待它加载结束 就可以继续执行后面的代码了
+                while (abDic[strs[i]] == null)
+                {
+                    //只要发现正在加载中 就不停的等待一帧 下一帧再进行判断
+                    yield return 0;
+                }
+            }
+        }
         //加载目标包
         if (!abDic.ContainsKey(abName))
         {
-            AssetBundle ab = AssetBundle.LoadFromFile(PathUrl + abName);
-            abDic.Add(abName, ab);
+            //一开始异步加载 就记录 如果此时的记录中的值 是null 那证明这个ab包正在被异步加载
+            abDic.Add(abName, null);
+            AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync(PathUrl + abName);
+            yield return req;
+            //异步加载结束后 再替换之前的null  这时 不为null 就证明加载结束了
+            abDic[abName] = req.assetBundle;
         }
+        else
+        {
+            //如果字典中记录的信息是null 那就证明正在加载中
+            //我们只需要等待它加载结束 就可以继续执行后面的代码了
+            while (abDic[abName] == null)
+            {
+                //只要发现正在加载中 就不停的等待一帧 下一帧再进行判断
+                yield return 0;
+            }
+        }
+
         //异步加载包中资源
         AssetBundleRequest abq = abDic[abName].LoadAssetAsync<T>(resName);
         yield return abq;
 
-        if (abq.asset is GameObject)
-            callBack(Instantiate(abq.asset) as T);
-        else
-            callBack(abq.asset as T);
+        callBack(abq.asset as T);
     }
 
     /// <summary>
@@ -213,22 +250,58 @@ public class ABMgr : SingletonAutoMono<ABMgr>
 
     private IEnumerator ReallyLoadResAsync(string abName, string resName, System.Type type, UnityAction<Object> callBack)
     {
-        //加载依赖包
-        LoadDependencies(abName);
+        //加载主包
+        LoadMainAB();
+        //获取依赖包
+        string[] strs = manifest.GetAllDependencies(abName);
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (!abDic.ContainsKey(strs[i]))
+            {
+                //一开始异步加载 就记录 如果此时的记录中的值 是null 那证明这个ab包正在被异步加载
+                abDic.Add(strs[i], null);
+                AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync(PathUrl + strs[i]);
+                yield return req;
+                //异步加载结束后 再替换之前的null  这时 不为null 就证明加载结束了
+                abDic[strs[i]] = req.assetBundle;
+            }
+            //就证明 字典中已经记录了一个AB包相关信息了
+            else
+            {
+                //如果字典中记录的信息是null 那就证明正在加载中
+                //我们只需要等待它加载结束 就可以继续执行后面的代码了
+                while (abDic[strs[i]] == null)
+                {
+                    //只要发现正在加载中 就不停的等待一帧 下一帧再进行判断
+                    yield return 0;
+                }
+            }
+        }
         //加载目标包
         if (!abDic.ContainsKey(abName))
         {
-            AssetBundle ab = AssetBundle.LoadFromFile(PathUrl + abName);
-            abDic.Add(abName, ab);
+            //一开始异步加载 就记录 如果此时的记录中的值 是null 那证明这个ab包正在被异步加载
+            abDic.Add(abName, null);
+            AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync(PathUrl + abName);
+            yield return req;
+            //异步加载结束后 再替换之前的null  这时 不为null 就证明加载结束了
+            abDic[abName] = req.assetBundle;
+        }
+        else
+        {
+            //如果字典中记录的信息是null 那就证明正在加载中
+            //我们只需要等待它加载结束 就可以继续执行后面的代码了
+            while (abDic[abName] == null)
+            {
+                //只要发现正在加载中 就不停的等待一帧 下一帧再进行判断
+                yield return 0;
+            }
         }
         //异步加载包中资源
         AssetBundleRequest abq = abDic[abName].LoadAssetAsync(resName, type);
         yield return abq;
 
-        if (abq.asset is GameObject)
-            callBack(Instantiate(abq.asset));
-        else
-            callBack(abq.asset);
+        callBack(abq.asset);
     }
 
     /// <summary>
@@ -244,37 +317,83 @@ public class ABMgr : SingletonAutoMono<ABMgr>
 
     private IEnumerator ReallyLoadResAsync(string abName, string resName, UnityAction<Object> callBack)
     {
-        //加载依赖包
-        LoadDependencies(abName);
+        //加载主包
+        LoadMainAB();
+        //获取依赖包
+        string[] strs = manifest.GetAllDependencies(abName);
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (!abDic.ContainsKey(strs[i]))
+            {
+                //一开始异步加载 就记录 如果此时的记录中的值 是null 那证明这个ab包正在被异步加载
+                abDic.Add(strs[i], null);
+                AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync(PathUrl + strs[i]);
+                yield return req;
+                //异步加载结束后 再替换之前的null  这时 不为null 就证明加载结束了
+                abDic[strs[i]] = req.assetBundle;
+            }
+            //就证明 字典中已经记录了一个AB包相关信息了
+            else
+            {
+                //如果字典中记录的信息是null 那就证明正在加载中
+                //我们只需要等待它加载结束 就可以继续执行后面的代码了
+                while (abDic[strs[i]] == null)
+                {
+                    //只要发现正在加载中 就不停的等待一帧 下一帧再进行判断
+                    yield return 0;
+                }
+            }
+        }
         //加载目标包
         if (!abDic.ContainsKey(abName))
         {
-            AssetBundle ab = AssetBundle.LoadFromFile(PathUrl + abName);
-            abDic.Add(abName, ab);
+            //一开始异步加载 就记录 如果此时的记录中的值 是null 那证明这个ab包正在被异步加载
+            abDic.Add(abName, null);
+            AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync(PathUrl + abName);
+            yield return req;
+            //异步加载结束后 再替换之前的null  这时 不为null 就证明加载结束了
+            abDic[abName] = req.assetBundle;
+        }
+        else
+        {
+            //如果字典中记录的信息是null 那就证明正在加载中
+            //我们只需要等待它加载结束 就可以继续执行后面的代码了
+            while (abDic[abName] == null)
+            {
+                //只要发现正在加载中 就不停的等待一帧 下一帧再进行判断
+                yield return 0;
+            }
         }
         //异步加载包中资源
         AssetBundleRequest abq = abDic[abName].LoadAssetAsync(resName);
         yield return abq;
 
-        if (abq.asset is GameObject)
-            callBack(Instantiate(abq.asset));
-        else
-            callBack(abq.asset);
+        callBack(abq.asset);
     }
 
     //卸载AB包的方法
-    public void UnLoadAB(string name)
+    public void UnLoadAB(string name, UnityAction<bool> callBackResult)
     {
         if( abDic.ContainsKey(name) )
         {
+            if (abDic[name] == null)
+            {
+                //代表正在异步加载 没有卸载成功
+                callBackResult(false);
+                return;
+            }
             abDic[name].Unload(false);
             abDic.Remove(name);
+            //卸载成功
+            callBackResult(true);
         }
     }
 
     //清空AB包的方法
     public void ClearAB()
     {
+        //由于AB包都是异步加载了 因此在清理之前 停止协同程序
+        StopAllCoroutines();
         AssetBundle.UnloadAllAssetBundles(false);
         abDic.Clear();
         //卸载主包
