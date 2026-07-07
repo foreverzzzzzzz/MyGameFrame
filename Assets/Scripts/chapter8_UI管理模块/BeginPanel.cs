ced7906751f57ce54911351cc8abb4e5e0ddac97
foreverzzzzzzz <15834063946@163.com>
2026-07-07 18:57:34 +0800

    UI管理模块-进阶优化自定义事件添加函数

--- Assets/Scripts/chapter8_UI管理模块/BeginPanel.cs
+++ Assets/Scripts/chapter8_UI管理模块/BeginPanel.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BeginPanel : BasePanel
 {
+    private void Start()
+    {
+        UIMgr.AddCustomEventListener(GetControl<Button>("btn"),EventTriggerType.PointerEnter, (data) =>
+        {
+            print("鼠标进入");
+        });
+    }
+
     public override void HideMe()
     {
         
